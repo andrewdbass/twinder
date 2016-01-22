5f292af248ea7ba87c1ceaa6972039a2eb755277
andrewdbass <andybass12@gmail.com>
2016-01-22 16:48:55 -0700

    Add twitter authetication

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-var app = angular.module('twitterFeed', ["firebase"]);
-app.controller('loginController',['$scope', '$window', 'firebaseArray', '$firebaseObject', function ($scope, $window, $firebaseArray, $firebaseObject){
-	var ref = new Firebase("http://twinder.firebaseio.com");
-	ref.authWithOAuthPopup("twitter", function(error, authData) {
-	  if (error) {
-	    console.log("Login Failed!", error);
-	  } else {
-	    console.log("Authenticated successfully with payload:", authData);
-	  }
+var app = angular.module('twinder', ["firebase"]);
+app.controller('LoginController',['$scope','$window', '$firebaseArray', '$firebaseObject', function($scope,$window,$firebaseArray, $firebaseObject){
+	var ref = new Firebase("https://twinder.firebaseio.com");
+	
+	$scope.login = function() {
+	ref.authWithOAuthPopup("twitter",function(error, authData) {
+		if(error){
+			$window.alert("Login Failed!" + error);
+		}
+		else{
+			$window.alert("Authenticated successfully with payload:" +authData)
+		}
 	});
+
+	};
+	
 }]);
+//https://drive.google.com/folderview?id=0B7mFI-OzUoSXWTFoQVh1SmFTa3c&usp=sharing
