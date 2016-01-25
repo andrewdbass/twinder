fc0509b8c5cc6364ef173107e29dcd9cd7b74bef
andrewdbass <andybass12@gmail.com>
2016-01-25 15:13:21 -0700

    make authData to load imediately

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 var app = angular.module('twinder', ["firebase"]);
-app.controller('LoginController',['$scope','$window', '$firebaseArray', '$firebaseObject', function($scope,$window,$firebaseArray, $firebaseObject){
-	var ref = new Firebase("https://twinder.firebaseio.com");
-	
-	$scope.login = function() {
-	ref.authWithOAuthPopup("twitter",function(error, authData) {
-		if(error){
-			$window.alert("Login Failed!" + error);
-		}
-		else{
-			$window.alert("Authenticated successfully with payload:" +authData)
-		}
-	});
+app.controller('LoginController',['$scope','$window', '$firebaseArray', '$firebaseObject', '$timeout', function($scope,$window,$firebaseArray, $firebaseObject, $timeout){
+	ref = new Firebase("https://twinder.firebaseio.com");
+	$scope.userData = ref.getAuth();
 
+	ref.onAuth(function(authData) {
+		$scope.userData = authData;
+		//this next part makes the data auto load
+		$timeout(function() {
+	       $scope.$apply();
+	    }, 500);				
+	});
+	$scope.login = function() {
+		ref.authWithOAuthPopup("twitter",function(error, authData) {});
 	};
-	
+	$scope.logout = function() {
+		ref.unauth();
+	};
+
 }]);
-//https://drive.google.com/folderview?id=0B7mFI-OzUoSXWTFoQVh1SmFTa3c&usp=sharing
