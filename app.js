530d967321e7a9719931cf0be04ef89db9223fd8
andrewdbass <andybass12@gmail.com>
2016-01-27 16:52:29 -0700

    create authFactory

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-var app = angular.module('twinder', ["firebase"]);
-app.controller('LoginController',['$scope','$window', '$firebaseArray', '$firebaseObject', '$timeout', function($scope,$window,$firebaseArray, $firebaseObject, $timeout){
-	ref = new Firebase("https://twinder.firebaseio.com");
-	$scope.userData = ref.getAuth();
+var app = angular.module('twinder', ["firebase", 'ngResource']);
 
-	ref.onAuth(function(authData) {
+app.controller('LoginController', function( $scope, $window, $timeout, $http, $q, $resource, authFactory){
+	var ref = new Firebase("https://twinder.firebaseio.com");
+	$scope.userData = authFactory.userData();
+	
+	//listener for change in auth
+	authFactory.ref.onAuth(function(authData) {
 		$scope.userData = authData;
 		//this next part makes the data auto load
 		$timeout(function() {
 	       $scope.$apply();
 	    }, 500);				
 	});
-	$scope.login = function() {
-		ref.authWithOAuthPopup("twitter",function(error, authData) {});
-	};
-	$scope.logout = function() {
-		ref.unauth();
-	};
 
-}]);
+	$scope.login = authFactory.login;
+	$scope.logout = authFactory.logout;
+
+	
+
+});
+
+app.factory('authFactory', function($timeout){
+	var service = {};
+	
+	service.ref = new Firebase("https://twinder.firebaseio.com");
+	var userData = {};
+	service.login = function(){
+		service.ref.authWithOAuthPopup("twitter",function(error, authData) {});
+	}
+	service.logout = function() {
+		service.ref.unauth();
+	};
+	service.userData = function(){
+		return service.ref.getAuth();
+	}
+	return service;
+});
